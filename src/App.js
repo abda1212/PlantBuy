95a61c12b0268803058bc4c0d37bbbbcc4ef8918
abdullahidayib <abdullahidayib03@gmail.com>
2024-12-21 21:45:36 +0100

    i have added some styling into categories

--- src/App.js
+++ src/App.js
@@ -3,29 +3,14 @@ import Categories from "./components/Categories";
 const App = () => {
   return (
     
-    <div className='categories-container'>
-   <Categories
+<div className="w-full flex flex-wrap justify-between gap-4">
+  <Categories title="Hats" imgsrc="'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPUL3pET-BblsLO62pglbzbKsVji5_2ysBng&s'" />
+  <Categories title="Sneakers" imgsrc="'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPUL3pET-BblsLO62pglbzbKsVji5_2ysBng&s'" />
+  <Categories title="Jackets" imgsrc="'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPUL3pET-BblsLO62pglbzbKsVji5_2ysBng&s'" />
+  <Categories title="Mens" isLarge imgsrc="'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPUL3pET-BblsLO62pglbzbKsVji5_2ysBng&s'" />
+  <Categories title="Womens" isLarge imgsrc="'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPUL3pET-BblsLO62pglbzbKsVji5_2ysBng&s'" />
+</div>
 
-    title={'hats'}
-   />
-   <Categories
-
-title={'men'}
-/>
-<Categories
-
-title={'woman'}
-/>
-<Categories
-
-title={'sneakers'}
-/>
-<Categories
-
-title={'hoodies'}
-/>
-
-    </div>
   );
 };
 
